dfa95fb0a4ad0bff509fc696171a91a94bbfa38b
sanjeev <sanjeevgaund2002@gmail.com>
2024-05-28 16:18:58 +0530

    Create Education Component

---
+++ src/components/Educations.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import GraduationCap from '../assets/Images/Cap.png'
+import JPDawerImage from '../assets/Images/JPDawer.jpg'
+import CBPatel from '../assets/Images/CBPatel.jpg'
+import { Box, Grid, Stack } from '@mui/material'
+
+
+const Educations = () => {
+
+
+    const data = [
+
+        {
+            College: "J.P.Dawer institute of Information Science and Technology",
+            Course: "Master Of Science (Information And Communication Technology ) [M.Sc. (I.C.T.)]",
+            Year: "Jul 2023 - Present",
+            CGPA: "",
+            CollegeImageUrl: JPDawerImage
+        },
+        {
+            College: "J.P.Dawer institute of Information Science and Technology",
+            Course: "Master Of Science (Information And Communication Technology ) [M.Sc. (I.C.T.)]",
+            Year: "Jul 2023 - Present",
+            CGPA: "",
+            CollegeImageUrl: JPDawerImage
+        },
+    ]
+
+    return (
+        <Box>
+            <Grid color={"darkgray"} container >
+                <Grid lg={4} container alignItems={"center"}>
+                    <Box>
+                        <img style={{ display: "block", margin: "auto" }} src={GraduationCap} width={"50%"} alt="" />
+                        <h1 align={"center"}>Education</h1>
+                    </Box>
+
+                </Grid>
+                <Grid container rowGap={2} lg={8} p={2}>
+                    <Grid container lg={12}>
+                        <Grid lg={4} >
+                            <img src={JPDawerImage} width={"100%"} alt="" />
+                        </Grid>
+                        <Grid p={2} lg={7}>
+                            <h3>J.P.Dawer institute of Information Science and Technology</h3>
+                            <p>Master Of Science (Information And Communication Technology ) [M.Sc. (I.C.T.)]</p><br />
+                            <p>Jul 2023 - Present</p>
+                        </Grid>
+                    </Grid>
+                    <Grid xs={12} lg={12}><p><hr /></p></Grid>
+                    <Grid container  lg={12}>
+                        <Grid lg={4} >
+                            <img src={CBPatel} width={"100%"} alt="" />
+                        </Grid>
+                        <Grid p={2} lg={7}>
+                            <h3>C B Patel Computer College</h3>
+                            <p>Bachelor Of Computer Application (B.C.A.) </p><br />
+                            <p>Jul 2019 - Feb 2023</p>
+                            <p>CGPA : 8.17</p>
+                        </Grid>
+                    </Grid>
+
+                </Grid>
+            </Grid>
+        </Box>
+    )
+}
+
+export default Educations
---
+++ src/components/Home.jsx
@@ -0,0 +1,52 @@
+import { Box, Grid, Stack } from '@mui/material'
+import React from 'react'
+import instagramIcon from '../assets/Images/instagram-logo.png'
+import githubIcon from '../assets/Images/github-logo.png'
+import linkedinIcon from '../assets/Images/linkedin-logo.png'
+import facebookIcon from '../assets/Images/facebook-logo.png'
+import ProfileImage from '../assets/Images/Profile.jpg'
+import Educations from './Educations'
+
+const Home = () => {
+    return (
+        <>
+            <Box className="Home" >
+                <Grid container mb={10}>
+                    <Grid sx={{ fontSize: { xs: "13px", lg: "18px", } }} container direction={"column"} lg={6} p={[5, 20]}>
+                        <h1>Hello, gays</h1>
+                        <h1> My name is <i>Sanjeev Gaund</i></h1>
+
+                        <h1>I am a <i>FullStack Developer</i> </h1>
+                        <Stack columnGap={2} mt={3} direction={"row"}>
+                            <a target='_blank' href='http://linkedin.com/in/thesanjeev'>
+                                <img width={40} height={40} src={linkedinIcon} alt="" />
+                            </a>
+
+                            <a target='_blank' href='http://github.com/sanjeev8052'>
+                                <img style={{ backgroundColor: "white", borderRadius: "50%", padding: "2px" }} width={40} height={40} src={githubIcon} alt="" />
+                            </a>
+                            <a target='_blank' href='https://www.instagram.com/san.1eev/'>
+                                <img width={40} height={40} src={instagramIcon} alt="" />
+                            </a>
+                            <a target='_blank' href='http://linkedin.com/in/thesanjeev'>
+                                <img width={40} height={40} src={facebookIcon} alt="" />
+                            </a>
+
+
+                        </Stack>
+                    </Grid>
+                    <Grid container justifyContent={"center"} alignItems={"center"} lg={6} p={5}>
+                        <Box bgcolor={"tomato"} p={"2px"}>
+                            <img src={ProfileImage} width={"300px"} alt="" />
+                        </Box>
+                    </Grid>
+                </Grid>
+                <Educations/>
+            </Box>
+            
+        </>
+
+    )
+}
+
+export default Home
